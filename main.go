69a1fbe686b6a05a7ab1c7d691e05b63391eb6f4
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2026-02-26 17:32:03 +0900

    Add initial implementation for Discord bot with URL replacement functionality

--- main.go
+++ main.go
@@ -2,19 +2,88 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"regexp"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	token := getToken()
+	session, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
 	}
+
+	session.AddHandler(Handler)
+
+	err = session.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	_ = session.Close()
+}
+
+func getToken() string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return ""
+	}
+
+	token := os.Getenv("TOKEN")
+	return token
+}
+
+func Handler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	/*
+		https://www.nicovideo.jp/watch/aaaabbbbb
+		↓
+		https://www.nicovideo.gay/watch/aaaabbbbb
+
+		delete: embed
+		send: replaced url
+
+		regular expression: https://www\.nicovideo\.jp/watch/([a-zA-Z0-9]+)
+	*/
+
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	re := regexp.MustCompile("https://www\\.nicovideo\\.jp/watch/([a-zA-Z0-9]+)")
+
+	if re.MatchString(m.Content) {
+		fmt.Println(m.Content)
+
+		_, err := s.ChannelMessageEditComplex(&discordgo.MessageEdit{
+			Channel: m.ChannelID,
+			ID:      m.ID,
+			Flags:   discordgo.MessageFlagsSupressEmbeds,
+		})
+
+		if err != nil {
+			fmt.Println("Error editing message:", err)
+		}
+
+		replaced := re.ReplaceAllString(m.Content, "https://www.nicovideo.gay/watch/$1")
+
+		_, err = s.ChannelMessageSend(m.ChannelID, replaced)
+		if err != nil {
+			fmt.Println("Error sending message:", err)
+			return
+		}
+	}
+
 }
